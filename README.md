0d685a8358ef99f6063009ca93026e6b4dc2a276
Jonathan Schneider <jkschneider@gmail.com>
2021-07-28 13:54:23 -0700

    UpdateImage

---
+++ README.md
@@ -0,0 +1,10 @@
+![Logo](https://github.com/openrewrite/rewrite/raw/main/doc/logo-oss.png)
+### Migrate CircleCI. Automatically.
+
+[![ci](https://github.com/openrewrite/rewrite-circleci/actions/workflows/ci.yml/badge.svg)](https://github.com/openrewrite/rewrite-circleci/actions/workflows/ci.yml)
+[![Apache 2.0](https://img.shields.io/github/license/openrewrite/rewrite-circleci.svg)](https://www.apache.org/licenses/LICENSE-2.0)
+[![Maven Central](https://img.shields.io/maven-central/v/org.openrewrite.recipe/rewrite-circleci.svg)](https://mvnrepository.com/artifact/org.openrewrite.recipe/rewrite-circleci)
+
+### What is this?
+
+This project implements [Rewrite recipes](https://github.com/openrewrite/rewrite) to perform CircleCI hygiene and migration tasks.
---
+++ src/main/java/org/openrewrite/gradle/UpdateImage.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2021 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.gradle;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.openrewrite.Option;
+import org.openrewrite.Recipe;
+import org.openrewrite.yaml.ChangeValue;
+
+@EqualsAndHashCode(callSuper = false)
+@Getter
+public class UpdateImage extends Recipe {
+
+    @Option(displayName = "Image",
+            description = "Image to use.",
+            example = "circleci/openjdk:jdk")
+    private final String image;
+
+    public UpdateImage(String image) {
+        this.image = image;
+        doNext(new ChangeValue("/jobs/build/machine/image",
+                image,
+                ".circleci/config.yml")
+        );
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Update CircleCI image";
+    }
+
+    @Override
+    public String getDescription() {
+        return "See the list of [pre-built CircleCI images](https://circleci.com/docs/2.0/circleci-images/).";
+    }
+}
